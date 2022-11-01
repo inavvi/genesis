72a9b351252031d9430aaa3824548a719e4def92
Ivan Votintsev <navvi@mail.ru>
2022-11-01 09:29:33 +0300

    Papercut

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+public class Main {
+    //0
+    //1
+    //01
+    //10
+    //010
+    //0101 //Some time later... ;-)
+    //10000110
+    //Ж
+
+    public static void main(String[] args) {
+        //Мечтатели и творцы
+        class Dream {
+
+            String a;
+            String b;
+            String c;
+            String d;
+            String e;
+            String f;
+            String g;
+
+            public Dream(String a, String b, String c, String d, String e, String f, String g) {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+                this.d = d;
+                this.e = e;
+                this.f = f;
+                this.g = g;}
+
+            public void startDream() {
+                String whitespace=" ";
+                String point=".";
+                System.out.println(a+whitespace+b+whitespace+c+whitespace+d+whitespace+e+whitespace+f+whitespace+g+point);}
+
+            public void checkСonstructiontDream() {
+                System.out.println("2 Проверка соответсвия мечты, чертежа и уже созданного...");
+                System.out.println("Люди не в силах понять, что есть что-то выше. Секты, коммунисты, фанатики, фашисты, Сексуальные меньшинства, скинхеды и нацисты Запугивают нас, свои диктуя манифесты...");
+                System.out.println("Америка, несущая России только ложь и блашь, припудренной под красоту, Продажной и пустой жизнью вливается в мозги наших юнцов, Пытаясь подменить наши устои, пытаясь изменить сущность русского мировоззрения...");
+                System.out.println("Это бумажный мир, рыночный балаган. Взрослые не по годам, поколение куплю-продам. За бумагу не стесняются идти по головам. Пропаганды спам, долбит по мозгам...");
+                System.out.println("10:33 на часах, aye Темные очки на глазах, aye Вороны кружат в облаках, aye Странные круги на полях, yeah");
+                System.out.println("Невольные в истерике, враги опять в Америке Железный занавес 2.0, военная полемика...");
+                System.out.println("Мозги утекли за кордон Шлюхи остались пить Дон Периньон Вор управляет вором Понимания ноль, но для них это норм...");
+                System.out.println("Сговор древних мудрецов поставил Мир на грань...");
+                System.out.println("Узурпаторов план - нас доить как коров Вечно править, меняясь местами Им не нужен народ, нужно больше рабов Перед ними чтоб мы пресмыкались...");
+                System.out.println("Будущее детей штыком упирается в спину. Мы строим для них дома из костей, кровь превращая в глину. Нам говорят: \"Вы должны все свои жизни Родине!\", Но мы же ведь более чем сложны, вроде бы...");
+                System.out.println("Перемен требуют наши сердца, Перемен требуют наши глаза, В нашем смехе и в наших слезах, И в пульсации вен Перемен! Мы ждем перемен...");
+                System.out.println("Внимание!!! Выроботка вариантов решения ошибок проектирования и брака в строительстве...");
+                System.out.println("危机 Don't panic! Cooperation! В бизнесе противостояние - это всегда прелюдия к дальнейшему партнерству. Рустам Тарико - Sega & Electronic Arts Story");
+                System.out.println("Думай своими мозгами В каменных джунглях игры без правил Нечего взять, зато можно оставить Что-то важное здесь новым людям на память...");
+                System.out.println("Imagine all the people living life in peace, you You may say I'm a dreamer But I'm not the only one...");
+                System.out.println("Who cares when someone's time runs out If a moment is all we are Or quicker, quicker Who cares if one more light goes out Well, I do...");
+                System.out.println("Do you feel cold and lost in desperation? You build up hope, but failure’s all you’ve known Remember all the sadness and frustration And let it go. Let it go...");
+                System.out.println("I was not mad at you I was not trying to tear you down The words that I could've used I was too scared to say out loud If I cannot break your fall I'll pick you up right off the ground If you felt invisible, I won't let you feel that now...");
+                System.out.println("Listen to your heart Those angel voices They'll sing to you They'll be your guide back home...");
+                System.out.println("Хранить традиции, Слушать истории. Или Ломать границы, И делать историю. И то, и другое, Это может быть правильный путь...");}
+
+            //Фиксация Dream
+        }
+
+        //class Virus {
+
+            //Своеобразная форма жизни - иногда рождаются случайно, а иногда по желанию...
+            //10000110
+            //Ж {
+            //10000000
+            //А
+            //10010111
+            //Ч
+            //00100100
+            //$}
+
+        // Фиксация Virus
+        //}
+
+        Dream predkiMechta = new Dream ("Широкий","простор", "для","мечты","и","для","жизни");
+
+        System.out.println("1 Вспомним оригинал!");
+        predkiMechta.startDream();
+        predkiMechta.checkСonstructiontDream();
+
+        //Продолжение следует!
+
+    }
+}
